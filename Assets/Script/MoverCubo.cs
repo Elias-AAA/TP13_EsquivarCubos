2448accba05b3f0fa0ef2cef719f881538dc121d
Elias-AAA <50029956@est.ort.edu.ar>
2026-05-11 12:06:36 -0300

    Initial commit

---
+++ Assets/Script/MoverCubo.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoverCubo : MonoBehaviour
+
+{
+    public float der = 1f;
+    public float izq = -1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.D)){
+            transform.Translate(der, 0, 0);
+        }
+
+        else if(Input.GetKeyDown(KeyCode.A)){
+             transform.Translate(izq, 0, 0);
+        }
+    }
+}
+
---
+++ Assets/Script/RomperCubo.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RomperCubo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision col){
+        Debug.Log( gameObject.name + " Colisiono con" + col.gameObject.name);
+       
+       if(col.gameObject.name == "CuboPlayer"){
+        Destroy(col.gameObject);
+        }
+        else if(col.gameObject.name == "piso"){
+            //reposition
+        }
+    }
+}
